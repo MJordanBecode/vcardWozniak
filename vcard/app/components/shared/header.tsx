64e5bb848add3455d91109a6a46759065e11ec58
Masy Jordan <masyjordan@gmail.com>
2025-03-05 22:39:03 +0100

    mobile/Ok

--- vcard/app/components/shared/header.tsx
+++ vcard/app/components/shared/header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function HeaderVcard(){
     return (
-<nav className="flex border border-[#adacaa] p-2 justify-center m-2 rounded-lg shadow-[10px_15px_8px_-5px_rgba(0,0,0,0.3)] bg-cover bg-center " style={{ backgroundImage: "url('/header/navbar/mythical-dragon-beast-anime-style.jpg')" }} >
+<nav className="flex border border-[#adacaa] p-2 justify-center m-2 rounded-lg  bg-cover bg-center " style={{ backgroundImage: "url('/header/navbar/mythical-dragon-beast-anime-style.jpg')" }} >
   <div className="w-[100px] h-[100px] rounded-full overflow-hidden justify-end">
     <Image
       width={100}
