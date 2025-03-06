93ae7c8bccfb49a4f0d92d595823e7cffccd4702
MasyJordan <masyjordan@gmail.com>
2025-03-06 14:03:58 +0100

    Version HtmlCss

--- vcard/app/components/shared/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function FooterVcard(){
-    return(
-        <div className="pt-4">
-            <p className="text-center text-gray-500 text-sm py-4">© 2025 MJProg. Tous droits réservés.
-            </p>
-        </div>
-    )
-}
